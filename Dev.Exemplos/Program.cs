6b99635a370e968e38b7486850c37b630a36aca6
Gabriel Almeida <gabriel.almeida@imagine.com.br>
2021-04-16 15:44:32 -0300

    Criando primeiras extensoes

---
+++ Dev.Exemplos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Dev.Extensions;
+
+namespace Dev.Exemplos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string teste = "10";
+            Console.WriteLine(teste.ToDecimal());
+
+            teste = null;
+            Console.WriteLine(teste.ToDecimalNull());
+
+            teste = null;
+            Console.WriteLine(teste.IsNull());
+
+            teste = "";
+            Console.WriteLine(teste.IsEmpty());
+
+            teste = null;
+            Console.WriteLine(teste.IsNullOrEmpty());
+        }
+    }
+}
